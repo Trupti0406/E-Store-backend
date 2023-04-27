36a8044317c6eba60c5c4d0fdcbb2a4a1f215f97
trupti0406 <truptivijayyadav27@gmail.com>
2023-04-28 01:12:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+
+const port = process.env.PORT || 5000;
+
+// To fectch variables from .env file
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+// To convert the form data into a json object inside req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Paypal api
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "SandBox");
+});
+
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Sever is running at http://localhost:${port}`);
+});
